89cda6e67be830b5b79f3ad35847a8444b55f233
chrisgibson <chris.gibson83@outlook.com>
2018-04-25 17:09:41 +1000

    lesson 10

---
+++ app/src/main/java/com/chrisgibson/smack/Utilities/Constants.kt
@@ -0,0 +1,6 @@
+package com.chrisgibson.smack.Utilities
+
+const val BASE_URL = "https://bit-smack.herokuapp.com/v1/"
+//const val BASE_URL = "http://10.0.2.2:3005/v1/"
+const val URL_REGISTER = "${BASE_URL}account/register"
+
