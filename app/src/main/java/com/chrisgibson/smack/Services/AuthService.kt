d0d7f6e94f7fa9777753ca0a90d651b0d1a7c48e
chrisgibson <chris.gibson83@outlook.com>
2018-04-25 16:06:57 +1000

    lesson 9

---
+++ app/src/main/java/com/chrisgibson/smack/Services/AuthService.kt
@@ -0,0 +1,15 @@
+package com.chrisgibson.smack.Services
+
+import android.content.Context
+import com.chrisgibson.smack.Utilities.URL_REGISTER
+
+object AuthService {
+
+    fun registeruser(context: Context, email: String, password: String, complete: (Boolean) -> Unit){
+
+        val url = URL_REGISTER
+        
+        val json
+
+    }
+}
