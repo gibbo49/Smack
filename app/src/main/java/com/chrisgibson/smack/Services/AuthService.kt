8ccfea1882953d3b366012ffc52574427858c0cb
chrisgibson <chris.gibson83@outlook.com>
2018-04-26 17:53:03 +1000

    lesson 12

--- app/src/main/java/com/chrisgibson/smack/Services/AuthService.kt
+++ app/src/main/java/com/chrisgibson/smack/Services/AuthService.kt
@@ -1,15 +1,132 @@
 package com.chrisgibson.smack.Services
 
 import android.content.Context
+import android.util.Log
+import android.widget.Toast
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.chrisgibson.smack.Utilities.URL_CREATE_USER
+import com.chrisgibson.smack.Utilities.URL_LOGIN
 import com.chrisgibson.smack.Utilities.URL_REGISTER
+import org.json.JSONException
+import org.json.JSONObject
 
 object AuthService {
 
-    fun registeruser(context: Context, email: String, password: String, complete: (Boolean) -> Unit){
+    var isLoggedIn = false
+    var userEmail = ""
+    var authToken = ""
 
-        val url = URL_REGISTER
-        
-        val json
 
+    fun registerUser(context: Context, email: String, password: String, complete: (Boolean) -> Unit){
+
+        val jsonBody = JSONObject()
+        jsonBody.put("email", email)
+        jsonBody.put("password", password)
+        val requestBody = jsonBody.toString()
+
+        val registerRequest = object : StringRequest(Method.POST, URL_REGISTER, Response.Listener {response ->
+            Toast.makeText(context, response, Toast.LENGTH_LONG).show()
+            complete(true)
+        }, Response.ErrorListener {error ->
+            Log.d ("ERROR", "Could not register user: $error")
+            Toast.makeText(context, "Could not register user: $error", Toast.LENGTH_LONG).show()
+            complete(false)
+        }){
+            override fun getBodyContentType(): String {
+                return "application/json; charset=utf-8"
+            }
+
+            override fun getBody(): ByteArray {
+                return requestBody.toByteArray()
+            }
+        }
+        Volley.newRequestQueue(context).add(registerRequest)
+    }
+
+    fun loginUser(context: Context,email: String,password: String,complete: (Boolean) -> Unit){
+
+        val jsonBody = JSONObject()
+        jsonBody.put("email", email)
+        jsonBody.put("password", password)
+        val requestBody = jsonBody.toString()
+
+        val loginRequest = object : JsonObjectRequest(Method.POST, URL_LOGIN,null,Response.Listener {response ->
+
+           try {
+               userEmail = response.getString("user")
+               authToken = response.getString("token")
+               isLoggedIn = true
+               Toast.makeText(context, "User logged in.", Toast.LENGTH_LONG).show()
+               complete(true)
+           } catch (e: JSONException) {
+               Log.d("JSON", "EXC:" + e.localizedMessage)
+               Toast.makeText(context, "Could not login user.", Toast.LENGTH_LONG).show()
+               complete(false)
+           }
+
+        }, Response.ErrorListener { error ->
+            Log.d ("ERROR", "Could not login user: $error")
+            Toast.makeText(context, "Could not login user: $error", Toast.LENGTH_LONG).show()
+            complete(false)
+
+        }) {
+            override fun getBodyContentType(): String {
+                return "application/json; charset=utf-8"
+            }
+            override fun getBody(): ByteArray {
+                return requestBody.toByteArray()
+            }
+        }
+        Volley.newRequestQueue(context).add(loginRequest)
+    }
+
+    fun createUser(context: Context,name: String, email: String, avatarName: String,avatarColor:String, complete: (Boolean) -> Unit){
+        val jsonBody = JSONObject()
+        jsonBody.put("name", name)
+        jsonBody.put("email", email)
+        jsonBody.put("avatarName", avatarName)
+        jsonBody.put("avatarColor", avatarColor)
+        val requestBody = jsonBody.toString()
+
+        val createRequest = object : JsonObjectRequest(Method.POST, URL_CREATE_USER, null, Response.Listener { response ->
+            try {
+                UserDataService.name = response.getString("name")
+                UserDataService.email= response.getString("email")
+                UserDataService.avatarName= response.getString("avatarName")
+                UserDataService.avatarColor= response.getString("avatarColor")
+                UserDataService.id=response.getString("_id")
+                complete(true)
+
+            }catch (e: JSONException){
+                Log.d("JSON", "EXC" + e.localizedMessage)
+                Toast.makeText(context, "Could not add user.", Toast.LENGTH_LONG).show()
+                complete(false)
+            }
+
+
+        }, Response.ErrorListener { error ->
+            Log.d ("ERROR", "Could not add user: $error")
+            Toast.makeText(context, "Could not add user: $error", Toast.LENGTH_LONG).show()
+            complete(false)
+        }) {
+            override fun getBodyContentType(): String {
+                return "application/json; charset=utf-8"
+            }
+
+            override fun getBody(): ByteArray {
+                return requestBody.toByteArray()
+            }
+
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers.put("Authorization", "Bearer $authToken")
+                return headers
+            }
+
+        }
+        Volley.newRequestQueue(context).add(createRequest)
     }
 }
---
+++ app/src/main/java/com/chrisgibson/smack/Services/UserDataService.kt
@@ -0,0 +1,11 @@
+package com.chrisgibson.smack.Services
+
+object UserDataService {
+    var id = ""
+    var avatarColor = ""
+    var avatarName = ""
+    var email = ""
+    var name = ""
+
+
+}
